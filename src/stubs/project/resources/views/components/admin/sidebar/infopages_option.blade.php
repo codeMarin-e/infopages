9dcab730e70ab2f483c340ca81a5485884e66f8c
Marin <marin@dev.nddesign.no>
2023-02-02 17:18:01 +0200

    to not see sidebar option if do not have view right

--- src/stubs/project/resources/views/components/admin/sidebar/infopages_option.blade.php
+++ src/stubs/project/resources/views/components/admin/sidebar/infopages_option.blade.php
@@ -1,7 +1,9 @@
-{{--   INFOPAGES --}}
-<li class="nav-item @if(request()->route()->named("{$whereIam}.infopages.*")) active @endif">
-    <a class="nav-link " href="{{route("{$whereIam}.infopages.index")}}">
-        <i class="fa fa-fw fa-users mr-1"></i>
-        <span>@lang("admin/infopages/infopages.sidebar")</span>
-    </a>
-</li>
+@if($authUser->can('view', \App\Models\Infopage::class))
+    {{--   INFOPAGES --}}
+    <li class="nav-item @if(request()->route()->named("{$whereIam}.infopages.*")) active @endif">
+        <a class="nav-link " href="{{route("{$whereIam}.infopages.index")}}">
+            <i class="fa fa-fw fa-users mr-1"></i>
+            <span>@lang("admin/infopages/infopages.sidebar")</span>
+        </a>
+    </li>
+@endif
